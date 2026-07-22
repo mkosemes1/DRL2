3f72db1f57e233b7d6db626f8bafb4ff8eaf02d2
Dar-rius <mohamedtine17@gmail.com>
2026-07-22 18:59:21 +0000

    update

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ config = {
 }
 
 
-train_config = TrainConfig(device="cuda:0", model_name="agriDrone", model_saved_path="./checkpoints", timestamp=10_000_000)
+train_config = TrainConfig(device="cuda:0", model_name="agriDrone", model_saved_path="./checkpoints", timestamp=1_000_000)
 ppo_config = PPOConfig(clip_eps=0.2, ent_coef=0.001)
 env = AgriDroneEnv(config)
 obs_dim = env.observation_space.shape
